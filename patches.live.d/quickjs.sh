c5ed34c8bd01292f1f7e1c81d0c818a4ccf3306b
Neruthes <i@neruthes.xyz>
2022-10-15 05:43:58 +0000

    refactor: add LP_VERSION_TABLE

--- patches.live.d/quickjs.sh
+++ patches.live.d/quickjs.sh
@@ -1,5 +1,12 @@
+###############################################################
+# External package: quickjs
+#
+# Environment variables:  MAKE_OPTS
+###############################################################
+
 # Download
-local QJSVER=2021-03-27
+# QJSVER=2021-03-27
+QJSVER="$(grep 'quickjs,' <<< "$LP_VERSION_TABLE" | head -n1 | cut -d, -f2)"
 DOWNLOAD -O "https://bellard.org/quickjs/quickjs-$QJSVER.tar.xz"
 DOWNLOAD -O "https://bellard.org/quickjs/quickjs-extras-$QJSVER.tar.xz"
 tar -pxvf quickjs-$QJSVER.tar.xz
